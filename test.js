acb1836d1e4a2e37af49bbcfcdab010d408e0fdc
andrepolischuk <andre.polischuk@gmail.com>
2015-09-22 13:22:48 +0300

    Add ava instead of mocha

--- test.js
+++ test.js
@@ -1,26 +1,28 @@
 'use strict';
+var test = require('ava');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var join = require('path').join;
 
-describe('wa', function () {
-  before(function (done) {
-    helpers.run(join(__dirname, './app'))
-      .inDir(join(__dirname, './temp'))
-      .withOptions({'skip-install': true})
-      .on('end', done);
-  });
+test.before(function (t) {
+  helpers.run(join(__dirname, './app'))
+    .inDir(join(__dirname, './temp'))
+    .withOptions({'skip-install': true})
+    .on('end', function () {
+      t.end();
+    });
+});
 
-  it('should generate files', function () {
-    assert.file([
-      '.editorconfig',
-      '.gitignore',
-      'src/index.css',
-      'src/index.js',
-      'index.html',
-      'package.json'
-    ]);
+test('should generate files', function (t) {
+  assert.file([
+    '.editorconfig',
+    '.gitignore',
+    'src/index.css',
+    'src/index.js',
+    'index.html',
+    'package.json'
+  ]);
 
-    assert.fileContent('package.json', /"name": "temp"/);
-  });
+  assert.fileContent('package.json', /"name": "temp"/);
+  t.end();
 });
